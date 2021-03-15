547c6684429d6c1dd8b07df2ee6a37fe392c689a
zaid halim <zaid_abd.alhalim@yahoo.com>
2021-03-15 05:27:44 +0200

    first commit

---
+++ src/contracts/Color.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.21 <0.7.5;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Color is ERC721{
+  string[] public colors;
+  mapping(string => bool) _colorExists;
+
+  constructor() ERC721("Color", "COLOR") public {
+  }
+
+  // E.G. color = "#FFFFFF"
+  function mint(string memory _color) public {
+    require(!_colorExists[_color]);
+    colors.push(_color);
+    uint _id = colors.length -1;
+    _mint(msg.sender, _id);
+    _colorExists[_color] = true;
+  }
+
+}
+
+
